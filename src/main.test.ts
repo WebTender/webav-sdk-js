75d0f3e823f6cc39a275cf1bd213cf3b950ddfc6
Brandon De Rose <brandon@webtender.host>
2023-10-24 01:15:03 +1300

    Added support for WebTender's signed api requests Changed build system to support common js and esm

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,49 +1,69 @@
 import { describe, it, expect, vi } from "vitest";
-import { createWebAV } from "./main";
+import WebAV, { createWebAV } from "./main";
+import { config } from 'dotenv';
 
-global.fetch = vi.fn();
+config();
+
+describe("WebAV SDK JS - main.ts", () => {
+
+  if (process.env.WEBTENDER_API_KEY && process.env.WEBTENDER_API_SECRET) {
+    const av = new WebAV();
+    it('Can get recent statuses', async () => {
+      const statuses = await av.getRecentStatuses();
+
+      expect(statuses.last_page).to.be.a('number')
+      expect(statuses.per_page).to.be.a('number')
+      expect(statuses.current_page).toEqual(1);
+      expect(statuses.data).to.be.an('array')
+    });
+  } else {
+
+    global.fetch = vi.fn();
+
+    it("Mock - Can get recent statuses", async () => {
+      const av = createWebAV();
+      av.setApiKey("fake-123", 'fake-secret');
+      // @ts-ignore
+      fetch.mockResolvedValue(
+        createFetchResponse(200, {
+          data: [],
+          last_page: 1,
+          per_page: 1,
+          current_page: 1,
+        })
+      );
+
+      const response = await av.getRecentStatuses();
+      expect(response).toEqual({
+        data: [],
+        last_page: 1,
+        per_page: 1,
+        current_page: 1,
+      });
+    });
+  }
+
+  it("it should require an API key", () => {
+    const av = createWebAV();
+    av.setApiKey("", "");
+
+    expect(() => av.getRecentStatuses()).rejects.toThrowError(
+      "API Key and Secret must be set"
+    );
+  });
+
+});
 
 function createFetchResponse(statusCode, data) {
   if (statusCode === 200) {
     return {
-      json: () => new Promise((resolve) => resolve(data)),
+      json: async () => data,
       status: statusCode,
     };
   }
 
   return {
-    json: () => new Promise((resolve, reject) => reject(data)),
+    json: async () => data,
+    status: statusCode,
   };
 }
-
-describe("WebAV SDK JS - main.ts", () => {
-  it("it should require an API key", () => {
-    const av = createWebAV();
-
-    expect(async () => await av.getRecentStatuses()).rejects.toThrowError(
-      "API key is not set"
-    );
-  });
-
-  it("Can get recent statuses", async () => {
-    const av = createWebAV();
-    av.setApiKey("fake-123");
-    // @ts-ignore
-    fetch.mockResolvedValue(
-      createFetchResponse(200, {
-        data: [],
-        last_page: 1,
-        per_page: 1,
-        current_page: 1,
-      })
-    );
-
-    const response = await av.getRecentStatuses();
-    expect(response).toEqual({
-      data: [],
-      last_page: 1,
-      per_page: 1,
-      current_page: 1,
-    });
-  });
-});
